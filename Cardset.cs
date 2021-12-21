913ef68fd1f273d16c5476abd4905b18fbdd9908
Andrzej Gierlak <andrzej.gierlak@capgemini.com>
2021-12-21 15:17:28 +0100

    Function checking Streigh (Strit) should work, remove exception later

--- Cardset.cs
+++ Cardset.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TexasHoldEm;
 
@@ -77,6 +78,52 @@ public class Cardest
         }
         return false;
     }
+
+    int StrenghtOfStreigh = 0;
+    //Strit po polsku: po kolei
+    //Powinno działać dobrze
+    private bool CheckIfStreigh(){
+        throw new NotImplementedException();
+        //sprawdz scenariusz z asem najmniejszy i najwiekszy
+        StrenghtOfStreigh = 0;
+        for(int i = 0; 4+i <13;i++){
+            if(
+            numberOfCards[0+i]>=1 && 
+            numberOfCards[1+i]>=1 && 
+            numberOfCards[2+i]>=1 &&
+            numberOfCards[3+i]>=1 &&
+            numberOfCards[4+i]>=1 
+            ){
+                StrenghtOfStreigh = i;
+                return true;
+            }       
+        }
+        if(
+            numberOfCards[9]>=1 && 
+            numberOfCards[10]>=1 && 
+            numberOfCards[11]>=1 &&
+            numberOfCards[12]>=1 &&
+            numberOfCards[0]>=1 
+            ){
+                StrenghtOfStreigh = 10;
+                return true;
+            }
+        return false;    
+    }
+
+    private bool CheckIfPair(){
+        throw new NotImplementedException();
+        for(int i=0; i <numberOfCards.Count;i++){
+            if(numberOfCards[i]==2){
+                //zapisz symbol do zmiennej okreslonego typu- typ silny, typ slaby
+                //dopisz do siły level siłę setu para
+                //jeżeli typ silny i typ slaby jest zajety to porownaj z mocą
+                //zwroc true
+            }
+        }
+    }
+
+
 
 
 }
