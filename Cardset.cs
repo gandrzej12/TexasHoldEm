a007a422e4aeafe88cc3951e0f22aa50a1f90070
Andrzej Gierlak <andrzej.gierlak@capgemini.com>
2021-12-22 08:51:39 +0100

    Create CheckIfPair() function, function checks all posible pairs and sets numberOfPair. Function will be used in future in TwoPair(), OnePair(), FullHouse() Not sure how, but i think code should be refactored- hard to read

--- Cardset.cs
+++ Cardset.cs
@@ -13,8 +13,13 @@ public class Cardest
 
     private CardSymbol symbolIfColor;
     int StrenghtOfStreigh = 0;
-    int WhichNumberOfFour=99;//zmienic na byte? ustawiac null zamiast 99
-    int WhichNumberOfThree=99;//zmienic na byte? ustawiac null zamiast 99
+    int WhichNumberOfFour = 99;//zmienic na byte? ustawiac null zamiast 99
+    int WhichNumberOfThree = 99;//zmienic na byte? ustawiac null zamiast 99
+
+    private byte? FirstPairNumber = null;
+    private byte? SecondPairNumber = null;
+    private byte? ThirdPairNumber = null;
+
 
     private List<int> numberOfCards = new List<int>();
 
@@ -66,102 +71,155 @@ public class Cardest
                     break;
             }
         }
-        if(needClubs>=5){
-            symbolIfColor= CardSymbol.CLUB;
+        if (needClubs >= 5)
+        {
+            symbolIfColor = CardSymbol.CLUB;
             return true;
         }
-        if(needDiamonds>=5){
-            symbolIfColor= CardSymbol.DIAMOND;
+        if (needDiamonds >= 5)
+        {
+            symbolIfColor = CardSymbol.DIAMOND;
             return true;
         }
-        if(needHearts>=5){
-            symbolIfColor= CardSymbol.HEART;
+        if (needHearts >= 5)
+        {
+            symbolIfColor = CardSymbol.HEART;
             return true;
         }
-        if(needSpades>=5){
-            symbolIfColor= CardSymbol.SPADE;
+        if (needSpades >= 5)
+        {
+            symbolIfColor = CardSymbol.SPADE;
             return true;
         }
         return false;
     }
 
-    
+
     //Strit po polsku: po kolei
     //Powinno działać dobrze
     //sprawdz scenariusz z asem najmniejszy i najwiekszy
-    private bool CheckIfStreigh(){
+    private bool CheckIfStreigh()
+    {
         StrenghtOfStreigh = 0;
-        for(int i = 0; 4+i <13;i++){
-            if(
-            numberOfCards[0+i]>=1 && 
-            numberOfCards[1+i]>=1 && 
-            numberOfCards[2+i]>=1 &&
-            numberOfCards[3+i]>=1 &&
-            numberOfCards[4+i]>=1 
-            ){
+        for (int i = 0; 4 + i < 13; i++)
+        {
+            if (
+            numberOfCards[0 + i] >= 1 &&
+            numberOfCards[1 + i] >= 1 &&
+            numberOfCards[2 + i] >= 1 &&
+            numberOfCards[3 + i] >= 1 &&
+            numberOfCards[4 + i] >= 1
+            )
+            {
                 StrenghtOfStreigh = i;
                 return true;
-            }       
-        }
-        if(
-            numberOfCards[9]>=1 && 
-            numberOfCards[10]>=1 && 
-            numberOfCards[11]>=1 &&
-            numberOfCards[12]>=1 &&
-            numberOfCards[0]>=1 
-            ){
-                StrenghtOfStreigh = 10;
-                return true;
             }
-        return false;    
+        }
+        if (
+            numberOfCards[9] >= 1 &&
+            numberOfCards[10] >= 1 &&
+            numberOfCards[11] >= 1 &&
+            numberOfCards[12] >= 1 &&
+            numberOfCards[0] >= 1
+            )
+        {
+            StrenghtOfStreigh = 10;
+            return true;
+        }
+        return false;
     }
 
-    
+
     //Kareta po polsku: cztery takie same figury
     //ok bo moze istniec tylko jedna czworka
-    private bool CheckIfFour(){
+    private bool CheckIfFour()
+    {
         throw new NotImplementedException();
-        for(int i = 0; i < numberOfCards.Count; i++){
-            if(numberOfCards[i]==4){
-                WhichNumberOfFour=i+1;
+        for (int i = 0; i < numberOfCards.Count; i++)
+        {
+            if (numberOfCards[i] == 4)
+            {
+                WhichNumberOfFour = i + 1;
                 return true;
             }
         }
-        WhichNumberOfFour=99;
+        WhichNumberOfFour = 99;
         return false;
     }
 
     //zostanie najsilniejsza trojka
-    private bool CheckIfThree(){
+    private bool CheckIfThree()
+    {
         throw new NotImplementedException();
-        bool thereIsThree=false;       
-        for(int i = 1; i < numberOfCards.Count; i++){
-            if(numberOfCards[i]==3){
-                WhichNumberOfThree=i+1;
-                thereIsThree=true;
+        bool thereIsThree = false;
+        for (int i = 1; i < numberOfCards.Count; i++)
+        {
+            if (numberOfCards[i] == 3)
+            {
+                WhichNumberOfThree = i + 1;
+                thereIsThree = true;
             }
         }
-        if(numberOfCards[0]==3){//Aces
-                WhichNumberOfThree=1;
-                thereIsThree=true;
+        if (numberOfCards[0] == 3)
+        {//Aces
+            WhichNumberOfThree = 1;
+            thereIsThree = true;
         }
         return thereIsThree;
     }
 
     //not implemented 4 real
-    private bool CheckIfPair(){
+    private bool CheckIfPair()
+    {
         throw new NotImplementedException();
-        for(int i=0; i <numberOfCards.Count;i++){
-            if(numberOfCards[i]==2){
-                //zapisz symbol do zmiennej okreslonego typu- typ silny, typ slaby
-                //dopisz do siły level siłę setu para
-                //jeżeli typ silny i typ slaby jest zajety to porownaj z mocą
-                //zwroc true
+        byte numberOfPairs = 0;
+        for (int i = 1; i < numberOfCards.Count; i++)
+        {
+            if (numberOfCards[i] == 2)
+            {
+                switch (numberOfPairs)
+                {
+                    case 0:
+                        FirstPairNumber = (byte?)(i + 1);
+                        numberOfPairs++;
+                        break;
+                    case 1:
+                        SecondPairNumber = (byte?)(i + 1);
+                        numberOfPairs++;
+                        break;
+                    case 2:
+                        ThirdPairNumber = (byte?)(i + 1);
+                        numberOfPairs++;
+                        break;
+                }
             }
         }
+        // ace case
+        if (numberOfCards[0] == 2)
+        {
+            switch (numberOfPairs)
+            {
+                case 0:
+                    FirstPairNumber = (byte?)(1);
+                    numberOfPairs++;
+                    break;
+                case 1:
+                    SecondPairNumber = (byte?)(1);
+                    numberOfPairs++;
+                    break;
+                case 2:
+                    ThirdPairNumber = (byte?)(1);
+                    numberOfPairs++;
+                    break;
+            }
+        }
+        if(numberOfPairs>0){
+            return true;
+        }
+        return false;
     }
 
-    
+
 
 
 
