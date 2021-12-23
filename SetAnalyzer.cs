dae1043ec91630ea66d7e8e16a9d8d81627b48b0
Andrzej Gierlak <andrzej.gierlak@capgemini.com>
2021-12-23 11:05:27 +0100

    Add new abstract class: SetAnalyzer. It shall be derived from incoming classes: Flush,Streigh, StreighFlush...

---
+++ SetAnalyzer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TexasHoldEm;
+
+public abstract class SetAnalyzer
+{
+    private List<Card> allCards = new List<Card>();
+    private List<Byte> cardSetLevels=new List<Byte>();
+
+    public SetAnalyzer(List<Card> someCards)
+    {
+        allCards=someCards;
+        ClearLevels();
+    }
+
+    protected void ClearLevels(){
+        cardSetLevels.Clear();
+        for (byte i = 0; i < 6; i++)
+        {
+            cardSetLevels.Add(0);
+        }
+    }
+
+    public byte GetLevel(byte level) => cardSetLevels[level];
+
+    public abstract bool CheckIfFits();
+    public abstract void calculateCardSetLevel();
+    
+}
