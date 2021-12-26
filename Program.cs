29fcdd2d53472c28b88a51729bc19cbfe9947b82
Andrzej Gierlak <andrzej.gierlak@capgemini.com>
2021-12-26 17:38:27 +0100

    It works!!!! ;D

--- Program.cs
+++ Program.cs
@@ -15,7 +15,23 @@ namespace TexasHoldEm
             // myDeck.PickACard().PrintCard();
 
             // new SetTester().readCSV();
-            new Game().StartGame();
+           // new Game().StartGame();
+           Deck ndeck=new Deck();
+           List<Card> lcard=new List<Card>();
+           lcard.Add(ndeck.PickACard());
+           lcard.Add(ndeck.PickACard());
+           lcard.Add(ndeck.PickACard());
+           lcard.Add(ndeck.PickACard());
+           lcard.Add(ndeck.PickACard());
+           SetAnalyzer sanal=new SetAnalyzer(lcard);
+           List<byte> levels=sanal.levelAnalyze();
+           foreach(var card in lcard){
+               card.PrintCard();
+           }
+           foreach (var level in levels){
+               Console.WriteLine($"Level: {level}");      
+           }
+                      
         }  
     }
 }
